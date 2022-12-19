76998d1491d49517d5c8e49022bf4fd71ec642be
Jae <giffordjacob0@gmail.com>
2022-12-19 15:57:41 +0800

    fetches data from new Schema appropriately

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,6 +8,7 @@ function Post({ postObj }) {
     post,
     published,
     title,
+    comments,
     updatedAt,
     user,
     _id,
@@ -15,15 +16,24 @@ function Post({ postObj }) {
   return (
     <div>
       <span>
-        <h1>{user}</h1>
+        <h1>{user.userName}</h1>
         <div>Options</div>
       </span>
       <div>
-        <img src={`${image.img.url}`}></img>
+        <img src={`${image.url}`}></img>
       </div>
       <span>
         <div>{like}</div>
-        <p>Comment</p>
+        <p>
+          {comments.map((el) => (
+            <div>
+              {' '}
+              COMMENTS -----
+              <p>{el.comment}</p>
+              <p>{el.user.userName}</p>
+            </div>
+          ))}
+        </p>
         <p>Share</p>
       </span>
       <span>Liked by ... usernames</span>
