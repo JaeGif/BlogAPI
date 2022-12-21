5e0776ba6e8d978bbee4713bd207f681a4e0bafa
Jae <giffordjacob0@gmail.com>
2022-12-21 13:37:34 +0800

    removed tailwind minor css update

--- src/components/comments/Comment.jsx
+++ src/components/comments/Comment.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 function Comment({ commentObj }) {
-  console.log(commentObj);
-
   return (
     <div>
       <span>
--- src/components/posts_components/Post.jsx
+++ src/components/posts_components/Post.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Comment from '../comments/Comment';
 import style from './post.module.css';
+import uniqid from 'uniqid';
 
 function Post({ postObj }) {
   const {
@@ -16,32 +17,33 @@ function Post({ postObj }) {
     _id,
   } = postObj;
   return (
-    <div>
+    <div className={style.postContainer}>
       <span className={style.userDateHead}>
-        <h1>{user.userName}</h1>
+        <h3>{user.userName}</h3>
         <h3>{createdAt}</h3>
-        <div>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            height='48'
-            width='48'
-            style={{ color: 'white' }}
-          >
-            <path d='M10.4 26.4q-1 0-1.7-.7T8 24q0-1 .7-1.7t1.7-.7q1 0 1.7.7t.7 1.7q0 1-.7 1.7t-1.7.7Zm13.6 0q-1 0-1.7-.7t-.7-1.7q0-1 .7-1.7t1.7-.7q1 0 1.7.7t.7 1.7q0 1-.7 1.7t-1.7.7Zm13.6 0q-1 0-1.7-.7t-.7-1.7q0-1 .7-1.7t1.7-.7q1 0 1.7.7T40 24q0 1-.7 1.7t-1.7.7Z' />
-          </svg>
+        <div className={style.optionsEllipses}>
+          <img
+            className={style.optionsEllipses}
+            src='./src/assets/favicons/horizontalellipse.svg'
+          ></img>
         </div>
       </span>
       <div>
         <img className={style.postImages} src={`${image.url}`}></img>
       </div>
       <span>
-        <div>{like}</div>
-        {comments.map((comment) => (
-          <Comment commentObj={comment} />
-        ))}
+        <div>
+          <img src='./src/assets/favicons/favorite.svg'></img>
+          {like}
+        </div>
         <p>Share</p>
       </span>
       <span>Liked by ... usernames</span>
+      <div>
+        {comments.map((comment) => (
+          <Comment key={uniqid()} commentObj={comment} />
+        ))}
+      </div>
     </div>
   );
 }
