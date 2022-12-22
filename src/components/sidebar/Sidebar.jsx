d009d98af700dcf16b21f398c3202c56aaf08ac0
Jae <giffordjacob0@gmail.com>
2022-12-22 14:20:18 +0800

    svgs added for optoins sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -22,6 +22,18 @@ function Sidebar() {
             <span className={style.optionSpan}>
               <img
                 className={style.optionsIcons}
+                src='./src/assets/favicons/search.svg'
+                alt='search'
+              />
+              <h2>Search</h2>
+            </span>
+          </a>
+        </div>
+        <div>
+          <a href='#'>
+            <span className={style.optionSpan}>
+              <img
+                className={style.optionsIcons}
                 src='./src/assets/favicons/account.svg'
                 alt='user account'
               />
@@ -30,12 +42,61 @@ function Sidebar() {
           </a>
         </div>
         <div>
+          <a href='#'>
+            <span className={style.optionSpan}>
+              <img
+                className={style.optionsIcons}
+                src='./src/assets/favicons/chat.svg'
+                alt='messages'
+              />
+              <h2>Messages</h2>
+            </span>
+          </a>
+        </div>
+        <div>
+          <a href='#'>
+            <span className={style.optionSpan}>
+              <img
+                className={style.optionsIcons}
+                src='./src/assets/favicons/favorite.svg'
+                alt='notifications'
+              />
+              <h2>Notifications</h2>
+            </span>
+          </a>
+        </div>
+        <div>
+          <a href='#'>
+            <span className={style.optionSpan}>
+              <img
+                className={style.optionsIcons}
+                src='./src/assets/favicons/add.svg'
+                alt='New Post'
+              />
+              <h2>Create</h2>
+            </span>
+          </a>
+        </div>
+        <div>
+          <a href='#'>
+            <span className={style.optionSpan}>
+              <img
+                className={style.optionsIcons}
+                src='./src/assets/favicons/fingerprint.svg'
+                alt='user account'
+              />
+              <h2>Profile</h2>
+            </span>
+          </a>
+        </div>
+
+        <div>
           <a href='https://github.com/JaeGif' target='_blank' rel='noreferrer'>
             <span className={style.optionSpan}>
               <img
                 className={style.optionsIcons}
                 src='./src/assets/github.png'
-                alt='user account'
+                alt='creators github'
               />
               <h2>Creator's Github</h2>
             </span>
