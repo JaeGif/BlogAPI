1ada96801c546593f260094d4d2ad1675fab12e0
Jae <giffordjacob0@gmail.com>
2022-12-23 13:42:12 +0800

    commentInput component now highlights post button when value is entered.

--- src/components/comments/addComment/addCommentInput.jsx
+++ src/components/comments/addComment/addCommentInput.jsx
@@ -3,15 +3,30 @@ import style from './AddCommentInput.module.css';
 
 function AddCommentInput() {
   const [isValue, setIsValue] = useState(false);
+
+  function checkValue(e) {
+    if (e.target.value !== '') {
+      setIsValue(true);
+    } else {
+      setIsValue(false);
+    }
+  }
   return (
     <span className={style.commentAreaContainer}>
       <form className={style.formElement}>
         <textarea
+          onChange={checkValue}
           className={style.commentInput}
           type='text'
           placeholder='Add a comment...'
         ></textarea>
-        <button className={style.postBtn}>Post</button>
+        <button
+          className={
+            isValue ? `${style.postBtn} ${style.activePost}` : style.postBtn
+          }
+        >
+          Post
+        </button>
       </form>
     </span>
   );
