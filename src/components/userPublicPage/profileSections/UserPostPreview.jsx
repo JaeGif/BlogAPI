0719c1be3730daaf23d880edd939b6ab511b8fc8
Jae <giffordjacob0@gmail.com>
2023-01-07 12:05:17 +0800

    content-type indicator added and css restyled with some variables

--- src/components/userPublicPage/profileSections/UserPostPreview.jsx
+++ src/components/userPublicPage/profileSections/UserPostPreview.jsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import style from './userpostpreview.module.css';
+import FullPost from '../../fullPost/FullPost';
 
 function UserPostPreview({ post }) {
   const apiURL = import.meta.env.VITE_RAILWAY_URL;
   const localURL = import.meta.env.VITE_LOCAL_URL;
   const [isVideo, setIsVideo] = useState(false);
+  const [displayPost, setDisplayPost] = useState(false);
+
+  const toggleDisplayFullPost = () => {
+    displayPost ? setDisplayPost(false) : setDisplayPost(true);
+  };
+  const updateParentPost = () => {
+    console.log('refresh?');
+  };
 
   useEffect(() => {
     if (post.image.contentType === 'video/mp4') {
@@ -13,12 +23,53 @@ function UserPostPreview({ post }) {
 
   return (
     <div>
-      {isVideo ? (
-        <video>
-          <source src={`${apiURL}/${post.image.url}`} type='video/mp4'></source>
-        </video>
+      <div
+        onClick={toggleDisplayFullPost}
+        className={style.squarePreviewContainer}
+      >
+        <div className={style.contentTypeIndicatorContainer}>
+          {isVideo ? (
+            <>
+              <span className={style.contentIndicatorWrapper}>
+                <img
+                  className={style.contentIndicatorIcon}
+                  src='./src/assets/favicons/movie.svg'
+                  alt='indicate video'
+                />
+              </span>
+              <video className={style.squarePreviewContent}>
+                <source
+                  src={`${apiURL}/${post.image.url}`}
+                  type='video/mp4'
+                ></source>
+              </video>
+            </>
+          ) : (
+            <>
+              <span className={style.contentIndicatorWrapper}>
+                <img
+                  className={style.contentIndicatorIcon}
+                  src='./src/assets/favicons/photo.svg'
+                  alt='indicate photo'
+                />
+              </span>
+              <img
+                className={style.squarePreviewContent}
+                src={`${apiURL}/${post.image.url}`}
+              />
+            </>
+          )}
+        </div>
+      </div>
+      {displayPost ? (
+        <FullPost
+          postObj={post}
+          toggleFullPost={toggleDisplayFullPost}
+          updateParentPost={updateParentPost}
+          isVideo={isVideo}
+        />
       ) : (
-        <img src={`${apiURL}/${post.image.url}`} />
+        <></>
       )}
     </div>
   );
--- src/components/userPublicPage/profileSections/UserPublished.jsx
+++ src/components/userPublicPage/profileSections/UserPublished.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import uniqid from 'uniqid';
 import UserPostPreview from './UserPostPreview';
+import style from './userpublished.module.css';
 
 function UserPublished({ isUserPage, user }) {
   const apiURL = import.meta.env.VITE_RAILWAY_URL;
@@ -29,11 +30,18 @@ function UserPublished({ isUserPage, user }) {
   }, [userPosts]);
 
   return (
-    <div>
+    <div className={style.contentLayoutGrid}>
       {postsFound ? (
         userPosts.map((post) => <UserPostPreview post={post} />)
       ) : (
-        <>Waiting</>
+        <>
+          Dev warning only waiting for post wtf is wrong something aint right
+          here maybe check on fetchAPI's string, did ya change the string Jae?
+          check that first always the problem handling 20 strings at once. Maybe
+          you oughta add somethign to the project to manage dev build and
+          production build? Nahhhhh just keep doing it this way Jae you're doing
+          juuuuust fine.
+        </>
       )}
     </div>
   );
