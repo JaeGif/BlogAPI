bdf11132afc33af3b03205d5be4c5c21831ace1b
Jae <giffordjacob0@gmail.com>
2023-01-09 17:10:40 +0800

    users are tagged by post id rather than full post now.

--- src/components/userPublicPage/profileSections/UserTagged.jsx
+++ src/components/userPublicPage/profileSections/UserTagged.jsx
@@ -6,8 +6,19 @@ import style from './userpublished.module.css';
 
 function UserTagged({ user }) {
   const apiURL = useContext(ApiContext);
-  const [userTagged, setUserTagged] = useState(user.taggedPosts);
+  const [userTaggedIdx, setUserTaggedIdx] = useState(user.taggedPosts);
+  const [userTagged, setUserTagged] = useState([]);
 
+  useEffect(() => {
+    for (let i = 0; i < userTaggedIdx.length; i++) {
+      fetchPostById(userTaggedIdx[i]);
+    }
+  }, [userTaggedIdx]);
+  const fetchPostById = async (id) => {
+    const res = await fetch(`${apiURL}/api/posts/${id}`);
+    const data = await res.json();
+    setUserTagged(userTagged.concat(data.post));
+  };
   return (
     <>
       {userTagged.length ? (
