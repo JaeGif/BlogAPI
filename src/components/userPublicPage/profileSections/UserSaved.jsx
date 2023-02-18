5814406f91f2e095ee970791c849573cb4fc5731
Jae <giffordjacob0@gmail.com>
2023-02-18 14:32:32 +0800

    saved posts correctly displayed again

--- src/components/userPublicPage/profileSections/UserSaved.jsx
+++ src/components/userPublicPage/profileSections/UserSaved.jsx
@@ -1,3 +1,4 @@
+import { useQueries, useQuery } from '@tanstack/react-query';
 import React, { useContext, useEffect, useState } from 'react';
 import uniqid from 'uniqid';
 import { ApiContext, TokenContext } from '../../../App';
@@ -8,41 +9,65 @@ import style from './userpublished.module.css';
 function UserPublished({ user }) {
   const apiURL = useContext(ApiContext);
   const token = useContext(TokenContext);
-
+  const [hasLength, setHasLength] = useState(false);
   const [userSavedIdx, setUserSavedIdx] = useState(user.savedPosts);
   const [userSaved, setUserSaved] = useState([]);
-  const [fetched, setFetched] = useState(false);
 
+  console.log(user.savedPosts);
+  useEffect(() => {
+    console.log('pass');
+    console.log(hasLength);
+    console.log(user.savedPosts.length);
+    if (user.savedPosts.length >= 0) {
+      setHasLength(true);
+    }
+  }, []);
   const fetchSavedPosts = async (id) => {
+    console.log(id);
+
     const res = await fetch(`${apiURL}/api/posts/${id}`, {
       mode: 'cors',
       headers: { Authorization: 'Bearer' + ' ' + token },
     });
     const data = await res.json();
-    setFetched(true);
+    console.log(data);
     return data.post;
   };
 
-  useEffect(() => {
+  /*   useEffect(() => {
     let userSavedPosts = [];
-    async function returnIntermediateData() {
-      for (let i = 0; i < userSavedIdx.length; i++) {
-        const post = await fetchSavedPosts(userSavedIdx[i]);
-        userSavedPosts.push(post);
+    if (userSavedIdx.length) {
+      async function returnIntermediateData() {
+        for (let i = 0; i < userSavedIdx.length; i++) {
+          const post = await fetchSavedPosts(userSavedIdx[i]);
+          userSavedPosts.push(post);
+        }
+        setUserSaved(userSaved.concat(userSavedPosts));
       }
-      setUserSaved(userSaved.concat(userSavedPosts));
+      console.log(userSavedIdx);
+      returnIntermediateData();
     }
-    returnIntermediateData();
-  }, []);
+  }, []); */
+  const savedPostsQueries = useQueries({
+    queries: user.savedPosts.map((postId) => {
+      return {
+        queryKey: ['posts', { postid: postId }],
+        queryFn: () => fetchSavedPosts(postId),
+        enabled: !!postId,
+      };
+    }),
+  });
 
-  console.log(userSaved);
+  console.log(hasLength);
+
+  console.log('saved,', savedPostsQueries);
   return (
     <>
-      {fetched ? (
-        userSaved.length ? (
+      {hasLength ? (
+        savedPostsQueries ? (
           <div className={style.contentLayoutGrid}>
-            {userSaved.map((post) => (
-              <UserPostPreview key={uniqid()} post={post} />
+            {savedPostsQueries.map((post) => (
+              <UserPostPreview key={uniqid()} post={post.data} />
             ))}
           </div>
         ) : (
